f59acde0bffa89dbae34d4f1905f1ffb1d0bf057
caiquemoa <caiquemoa@gmail.com>
2022-03-31 20:07:29 -0300

    heat info updated by forecast  hour

--- js/main.js
+++ js/main.js
@@ -4,6 +4,18 @@ const search = document.getElementById('search')
 
 const button = document.getElementById('button')
 
+const data = new Date()
+
+const dia = data.getDate() // 1-31
+
+const dia_sem = data.getDay() // 0-6 (zero=domingo)
+
+const mes = data.getMonth() // 0-11 (zero=janeiro)
+
+const hora = data.getHours() // 0-23
+
+const min = data.getMinutes() // 0-59
+
 const key = 'a3028f94c76d40369cf234124223003'
 wheatherLocation('pirapora')
 
@@ -23,8 +35,10 @@ function wheatherLocation(cidade) {
     .then(response => response.json())
     .then(wheather => {
       console.log(wheather)
+
       const { temp_c, condition, precip_mm, wind_kph, humidity } =
-        wheather.current
+        wheather.forecast.forecastday[0].hour[hora]
+      console.log(hora)
 
       const { forecastday } = wheather.forecast
 
@@ -40,13 +54,13 @@ function wheatherLocation(cidade) {
       currentHumidity.textContent = humidity + '%'
 
       currentWind.textContent = wind_kph + ' km/h'
-      forecastday.shift()
       for (let index = 0; index < forecastday.length; index++) {
-        let day = new Date(forecastday[index].date)
+        let day = new Date(forecastday[index + 1].date)
+        console.log(forecastday[index].date)
         forecastWeekDay[index].textContent = diaDaSemana[day.getUTCDay()]
-        forecastIcon[index].src = forecastday[index].day.condition.icon
-        maxTemp[index].textContent = forecastday[index].day.maxtemp_c + '°'
-        minTemp[index].textContent = forecastday[index].day.mintemp_c + '°'
+        forecastIcon[index].src = forecastday[index + 1].day.condition.icon
+        maxTemp[index].textContent = forecastday[index + 1].day.maxtemp_c + '°'
+        minTemp[index].textContent = forecastday[index + 1].day.mintemp_c + '°'
       }
     })
     .catch(error => {
@@ -55,19 +69,10 @@ function wheatherLocation(cidade) {
 }
 
 setInterval(() => {
-  let data = new Date()
-
-  let dia = data.getDate() // 1-31
-  let dia_sem = data.getDay() // 0-6 (zero=domingo)
-  let mes = data.getMonth() // 0-11 (zero=janeiro)
-
-  let hora = data.getHours() // 0-23
-  let min = data.getMinutes() // 0-59
-
-  let str_data = diaDaSemana[dia_sem] + ', ' + dia + ' ' + mesDoAno[mes % 12]
-  let str_hora = hora + ':' + min
+  const str_data = diaDaSemana[dia_sem] + ', ' + dia + ' ' + mesDoAno[mes % 12]
+  const str_hora = hora + ':' + min
   dateTime.textContent = str_data + ' ' + str_hora
-}, 10000)
+}, 1000)
 
 const diaDaSemana = [
   'Domingo',
