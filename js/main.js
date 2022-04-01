8591723e769908a04a4db64a5935ddc58b8eff72
caiquemoa <caiquemoa@gmail.com>
2022-04-01 14:08:54 -0300

    main.js changes search with enter and other changes

--- js/main.js
+++ js/main.js
@@ -1,35 +1,33 @@
-const dateTime = document.getElementById('dateTime')
-
-const search = document.getElementById('search')
-
-const button = document.getElementById('button')
-
 const data = new Date()
-
-const dia = data.getDate() // 1-31
-
-const dia_sem = data.getDay() // 0-6 (zero=domingo)
-
-const mes = data.getMonth() // 0-11 (zero=janeiro)
 
 const hora = data.getHours() // 0-23
 
-const min = data.getMinutes() // 0-59
+const forecastLength = 7
 
-const key = 'a3028f94c76d40369cf234124223003'
-wheatherLocation('pirapora')
+wheatherLocation()
 
 const forecastWeekDay = document.querySelectorAll('.forecastWeekDay')
+
 const forecastIcon = document.querySelectorAll('.forecastIcon')
+
 const maxTemp = document.querySelectorAll('.forecastMaxTemp')
+
 const minTemp = document.querySelectorAll('.forecastMinTemp')
+
+search.addEventListener('keyup', event => {
+  if (event.key === 'Enter' && search.value.length > 0) {
+    wheatherLocation(search.value)
+  }
+})
 
 button.addEventListener('click', () => {
   wheatherLocation(search.value)
 })
 
-function wheatherLocation(cidade) {
-  const url = `http://api.weatherapi.com/v1/forecast.json?key=${key}&q=${cidade}&days=7&aqi=yes&alerts=yes`
+function wheatherLocation(cidade = 'nova york') {
+  const key = 'a3028f94c76d40369cf234124223003'
+
+  const url = `http://api.weatherapi.com/v1/forecast.json?key=${key}&q=${cidade}&days=${forecastLength}&aqi=yes&alerts=yes&lang=pt`
 
   fetch(url)
     .then(response => response.json())
@@ -38,29 +36,33 @@ function wheatherLocation(cidade) {
 
       const { temp_c, condition, precip_mm, wind_kph, humidity } =
         wheather.forecast.forecastday[0].hour[hora]
-      console.log(hora)
 
       const { forecastday } = wheather.forecast
 
       cityName.textContent = wheather.location.name
 
-      mainTemp.textContent = Math.floor(temp_c) + '°'
+      mainTemp.textContent = `${Math.floor(temp_c)}°`
+
       conditionIcon.src = condition.icon
 
       conditionText.textContent = condition.text
 
-      currentPrecipitation.textContent = precip_mm + '%'
+      currentPrecipitation.textContent = `${precip_mm}%`
 
-      currentHumidity.textContent = humidity + '%'
+      currentHumidity.textContent = `${humidity}%`
 
-      currentWind.textContent = wind_kph + ' km/h'
+      currentWind.textContent = `${wind_kph} km/h`
+
       for (let index = 0; index < forecastday.length; index++) {
-        let day = new Date(forecastday[index + 1].date)
-        console.log(forecastday[index].date)
-        forecastWeekDay[index].textContent = diaDaSemana[day.getUTCDay()]
+        let data = new Date(forecastday[index + 1].date)
+
+        forecastWeekDay[index].textContent = diaDaSemana[data.getUTCDay()]
+
         forecastIcon[index].src = forecastday[index + 1].day.condition.icon
-        maxTemp[index].textContent = forecastday[index + 1].day.maxtemp_c + '°'
-        minTemp[index].textContent = forecastday[index + 1].day.mintemp_c + '°'
+
+        maxTemp[index].textContent = `${forecastday[index + 1].day.maxtemp_c}°`
+
+        minTemp[index].textContent = `${forecastday[index + 1].day.mintemp_c}°`
       }
     })
     .catch(error => {
@@ -69,9 +71,17 @@ function wheatherLocation(cidade) {
 }
 
 setInterval(() => {
-  const str_data = diaDaSemana[dia_sem] + ', ' + dia + ' ' + mesDoAno[mes % 12]
-  const str_hora = hora + ':' + min
-  dateTime.textContent = str_data + ' ' + str_hora
+  const dia = data.getDate() // 1-31
+
+  const dia_sem = data.getDay() // 0-6 (zero=domingo)
+
+  const mes = data.getMonth() // 0-11 (zero=janeiro)
+
+  const min = data.getMinutes() // 0-59
+
+  dateTime.textContent = `${diaDaSemana[dia_sem]}, ${dia} ${
+    mesDoAno[mes % 12]
+  }, ${hora}:${min}`
 }, 1000)
 
 const diaDaSemana = [
